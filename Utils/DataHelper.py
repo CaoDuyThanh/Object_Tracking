f82cf3fdfa9072e5d20e53580130da328e49227a
caoduythanh <caoduythanhcantho@gmail.com>
2017-05-11 14:24:28 +0700

    Modified: - Add option to save information about dataset

--- Utils/DataHelper.py
+++ Utils/DataHelper.py
@@ -2,6 +2,17 @@ import cv2
 import numpy
 
 class DatasetHelper:
+    def __init__(self):
+        self.DataOpts = {}
+
+        # Default setting for retrieve data
+        self.DataOpts['data_phase']       = 'train'     # Setting 'train' for training
+                                                        #         'test' for testing
+        self.DataOpts['data_folder_name'] = ''
+        self.DataOpts['data_folder_type'] = 'gt'        # Setting 'gt' for tracking
+                                                        # Setting 'det' for detection
+        self.DataOpts['data_object_id']   = '0'
+
     # ------------------------  LOAD TRAIN | VALID | TEST FILES--------------------------------------------
     def loadTrainFile(self): raise NotImplementedError
 
