064b4154efd81e4e0c954c8db7f0bcc7cc611f18
caoduythanh <caoduythanhcantho@gmail.com>
2017-05-11 14:32:27 +0700

    Modified: - Change the way calculate softmax layer with keep dimension option

--- Layers/SoftmaxLayer.py
+++ Layers/SoftmaxLayer.py
@@ -3,8 +3,13 @@ import numpy
 
 class SoftmaxLayer():
     def __init__(self,
+                 net,
                  input):
         # Save information to its layer
+        self.Axis    = net.LayerOpts['softmax_axis']
         self.Input   = input
 
-        self.Output = T.nnet.softmax(input)
+        e_x = T.exp(input - input.max(axis = self.Axis, keepdims = True))
+        out = e_x / e_x.sum(axis = self.Axis, keepdims = True)
+
+        self.Output = out
