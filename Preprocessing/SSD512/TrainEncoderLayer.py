474bcb62301721ffbbea4d95670fa40b3b2ba705
caoduythanh <caoduythanhcantho@gmail.com>
2017-05-15 00:41:58 +0700

    Modified: - Add hyper parameter of layer name

--- Preprocessing/SSD512/TrainEncoderLayer.py
+++ Preprocessing/SSD512/TrainEncoderLayer.py
@@ -2,6 +2,7 @@ import io
 import matplotlib.pyplot as plt
 import skimage.transform
 import urllib
+from Utils.FileHelper import *
 from os import listdir
 from os.path import abspath, join, isdir, isfile
 from Models.CustomSSD.SSD512CustomModel import *
@@ -9,17 +10,21 @@ from Utils.FilterHelper import *
 from PIL import Image
 
 
-NUM_EPOCH  = 10
+BATCH_SIZE = 1
+NUM_EPOCH  = 4
 NUM_SHOW   = 100
 NUM_SAVE   = 1000
-SAVE_MODEL = 'ssd512_conv6_2_encode.pkl'
+SAVE_MODEL = 'ssd512_conv4_3_encode.pkl'
+LAYER_NAME = 'conv4_3_norm_encode'
+LAYER_W_SIZE = (512, 256)
+IMG_SIZE     = (16, 32)
 
 # Model
 customSSDModel = None
 
 def LoadMode():
     global customSSDModel
-    customSSDModel = SSD512CustomModel(batchSize = 1)
+    customSSDModel = SSD512CustomModel(batchSize = BATCH_SIZE)
     customSSDModel.LoadCaffeModel('../../Models/SSD_512x512/VOC0712/deploy.prototxt',
                                   '../../Models/SSD_512x512/VOC0712/VGG_coco_SSD_512x512_iter_360000.caffemodel')
 
@@ -63,22 +68,38 @@ def TrainEncoderLayer():
     VGG_MEAN = numpy.asarray([103.939, 116.779, 123.68])
     VGG_MEAN = numpy.reshape(VGG_MEAN, (1, 3, 1, 1))
 
+    file = open('ssd512_conv4_3_encode.pkl')
+    customSSDModel.LoadLayers(file=file, layerNames=['conv4_3_norm_encode'])
+    file.close()
+    print('Load layer!')
+
+    # # Show all fileters from model
+    # image = Image.fromarray(tile_raster_images(
+    #     X=customSSDModel.Net.Layer['conv4_3_norm_encode'].W.get_value(borrow=False).reshape((512, 256)).T,
+    #     img_shape=(16, 32), tile_shape=(10, 10),
+    #     tile_spacing=(1, 1)))
+    # image.save('filters_corruption_30_0.png')
+
     # Load model
-    # file = open(SAVE_MODEL)
-    # customSSDModel.LoadLayers(file=file, layerNames=['conv6_2_encode'])
-    # file.close()
-    # print('Load layer!')
+    if CheckFileExist(SAVE_MODEL,
+                      throwError=False) == True:
+        file = open(SAVE_MODEL)
+        customSSDModel.LoadLayers(file=file, layerNames=[LAYER_NAME])
+        file.close()
+        print('Load layer!')
 
     # Show all fileters from model
     image = Image.fromarray(tile_raster_images(
-        X=customSSDModel.Net.Layer['conv6_2_encode'].W.get_value(borrow = False).reshape((512, 256)).T,
-        img_shape=(16, 32), tile_shape=(10, 10),
+        X=customSSDModel.Net.Layer[LAYER_NAME].W.get_value(borrow = False).reshape(LAYER_W_SIZE).T,
+        img_shape=IMG_SIZE, tile_shape=(10, 10),
         tile_spacing=(1, 1)))
     image.save('filters_corruption_30.png')
 
     allFilesPath = ListImage('/media/badapple/Data/PROJECTS/Machine Learning/Dataset/VOC/VOC2007/Train/JPEGImages')
     iter  = 0
     costs = []
+    imBatch = numpy.zeros((BATCH_SIZE, 3, 512, 512), dtype='float32')
+    idx     = 0
     for epoch in range(NUM_EPOCH):
         for url in allFilesPath:
             im = prep_image(url)
@@ -89,28 +110,36 @@ def TrainEncoderLayer():
             im = im[:, :, [2, 1, 0]]
             im = numpy.transpose(im, (2, 0, 1))
             im = numpy.reshape(im, (1, 3, 512, 512))
-            im = numpy.asarray(im - VGG_MEAN, dtype=numpy.float32)
 
-            cost = customSSDModel.Conv6_2EncodeFunc(im)
-            costs.append(cost)
+            if idx < BATCH_SIZE:
+                imBatch[idx, :, :, :] = im
+                idx += 1
 
-            if iter % NUM_SHOW == 0:
-                print('Epoch = %d, iteration = %d, cost = %f' % (epoch, iter, numpy.mean(costs)))
-                costs = []
+            if idx == BATCH_SIZE:
+                imBatch = numpy.asarray(imBatch - VGG_MEAN, dtype = 'float32')
 
-            if iter % NUM_SAVE == 0:
-                # Save model
-                file = open(SAVE_MODEL, 'wb')
-                customSSDModel.SaveLayers(file=file, layerNames=['conv6_2_encode'])
-                file.close()
-                print('Save model!')
+                cost = customSSDModel.Conv4_3EncodeFunc(imBatch)
+                costs.append(cost)
 
-            iter = (iter + 1) % allFilesPath.__len__()
+                if iter % NUM_SHOW == 0:
+                    print('Epoch = %d, iteration = %d, cost = %f' % (epoch, iter, numpy.mean(costs)))
+                    costs = []
+
+                if iter % NUM_SAVE == 0:
+                    # Save model
+                    file = open(SAVE_MODEL, 'wb')
+                    customSSDModel.SaveLayers(file=file, layerNames=[LAYER_NAME])
+                    file.close()
+                    print('Save model!')
+
+                iter = (iter + 1) % allFilesPath.__len__()
+
+                idx  = 0
 
         # Show all fileters from model
         image = Image.fromarray(tile_raster_images(
-            X=customSSDModel.Net.Layer['conv6_2_encode'].W.get_value(borrow=False).reshape((512, 256)).T,
-            img_shape=(16, 32), tile_shape=(10, 10),
+            X=customSSDModel.Net.Layer[LAYER_NAME].W.get_value(borrow=False).reshape(LAYER_W_SIZE).T,
+            img_shape=IMG_SIZE, tile_shape=(10, 10),
             tile_spacing=(1, 1)))
         image.save('filters_corruption_30.png')
 
