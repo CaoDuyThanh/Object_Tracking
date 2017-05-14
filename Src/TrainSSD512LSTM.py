87e6adba5ec16bdcfffdbde50e8a6d843787249e
caoduythanh <caoduythanhcantho@gmail.com>
2017-05-15 00:40:42 +0700

    Modified: - Add train with batch size - Change entire model to predict heatmap

--- Src/TrainSSD512LSTM.py
+++ Src/TrainSSD512LSTM.py
@@ -13,12 +13,13 @@ from Utils.BBoxHelper import *
 #                                                                                                                      #
 ########################################################################################################################
 # TRAINING HYPER PARAMETER
+BATCH_SIZE        = 5
 NUM_EPOCH         = 10
-DISPLAY_FREQUENCY = 10;     INFO_DISPLAY = 'Epoch = %d, iteration = %d, cost = %f, folderName = %s, objectId = %d'
-SAVE_FREQUENCY    = 150
+DISPLAY_FREQUENCY = 5;     INFO_DISPLAY = 'Epoch = %d, iteration = %d, cost = %f'
+SAVE_FREQUENCY    = 100
 
 # LSTM NETWORK CONFIG
-NUM_TRUNCATE      = 5
+NUM_TRUNCATE      = 8
 NUM_HIDDEN        = 512
 INPUTS_SIZE       = [257]
 OUTPUTS_SIZE      = [1]
@@ -37,8 +38,8 @@ SAVE_PATH       = '../Pretrained/SSD512/LSTM_SSD_Epoch=%d_Iter=%d.pkl'
 
 # LOAD MODEL PATH
 LOAD_MODEL_PATH = '../Pretrained/SSD512/LSTM_SSD_Epoch=%d_Iter=%d.pkl'
-START_EPOCH     = 0
-START_ITERATION = 0
+START_EPOCH     = 3
+START_ITERATION = 8800
 
 #  GLOBAL VARIABLES
 Dataset           = None
@@ -66,7 +67,7 @@ def LoadDataset():
 ########################################################################################################################
 def CreateSSDExtractFactory():
     global FeatureFactory, DefaultBboxs, BoxsVariances
-    FeatureFactory = SSD512FeaExtraction(batchSize = NUM_TRUNCATE + 1)
+    FeatureFactory = SSD512FeaExtraction()
     FeatureFactory.LoadCaffeModel('../Models/SSD_512x512/VOC0712/deploy.prototxt',
                                   '../Models/SSD_512x512/VOC0712/VGG_coco_SSD_512x512_iter_360000.caffemodel')
     FeatureFactory.LoadEncodeLayers('../Preprocessing/SSD512/ssd512_conv4_3_norm_encode.pkl',
@@ -97,7 +98,8 @@ def CreateSSDExtractFactory():
 ########################################################################################################################
 def CreateLSTMModel():
     global LSTMModel
-    LSTMModel = LSTMTrackingModel(numTruncate = NUM_TRUNCATE,
+    LSTMModel = LSTMTrackingModel(batchSize   = BATCH_SIZE,
+                                  numTruncate = NUM_TRUNCATE,
                                   numHidden   = NUM_HIDDEN,
                                   inputsSize  = INPUTS_SIZE,
                                   outputsSize = OUTPUTS_SIZE)
@@ -189,8 +191,8 @@ def TrainModel():
     global Dataset, LSTMModel, FeatureFactory, DefaultBboxs, BoxsVariances
 
     # Create startStateS | startStateC
-    startStateS = numpy.zeros((LSTMModel.Net.LayerOpts['lstm_num_hidden'],), dtype = 'float32')
-    startStateC = numpy.zeros((LSTMModel.Net.LayerOpts['lstm_num_hidden'],), dtype = 'float32')
+    startStateS = numpy.zeros((BATCH_SIZE, LSTMModel.Net.LayerOpts['lstm_num_hidden'],), dtype = 'float32')
+    startStateC = numpy.zeros((BATCH_SIZE, LSTMModel.Net.LayerOpts['lstm_num_hidden'],), dtype = 'float32')
 
     # Plot training cost
     iterVisualize = []
@@ -207,6 +209,8 @@ def TrainModel():
         file.close()
         print ('Load model !')
 
+    S = startStateS; C = startStateC
+
     # Train each folder in train folder
     iter = START_ITERATION
     costs = []
@@ -222,48 +226,84 @@ def TrainModel():
             Dataset.DataOpts['data_folder_type'] = 'gt'
             allObjectIds = Dataset.GetAllObjectIds()
 
-            for objectId in allObjectIds:
-                Dataset.DataOpts['data_object_id'] = objectId
+            # if epoch == 2 and folderName != 'MOT16-10':
+            #     continue
 
-                print ('Load metadata of objectId = %d' % (objectId))
-                imsPath, bboxs = Dataset.GetSequenceBy(occluderThres = 0.5)
+            numBatchObjectIds = allObjectIds.__len__() // BATCH_SIZE
+            for batchObjectIdx in range(numBatchObjectIds):
+                # if epoch == 2 and batchObjectIdx < 8:
+                #     continue
 
-                # If number image in sequence less than NUM_TRUNCATE => we choose another sequence to train
-                if (imsPath.__len__() < NUM_TRUNCATE + 1):
-                    continue
+                # Get batch of object ids
+                batchObjectIds = allObjectIds[batchObjectIdx * BATCH_SIZE : (batchObjectIdx + 1) * BATCH_SIZE]
 
-                # Else train the sequence ...................
-                S = startStateS;    C = startStateC
-                NUM_BATCH = (imsPath.__len__()) // NUM_TRUNCATE
-                for batchId in range(NUM_BATCH):
-                    print ('\r Load mini sequence !.....................')
-                    # Get batch
-                    imsPathSequence = imsPath[batchId * NUM_TRUNCATE : (batchId + 1) * NUM_TRUNCATE + 1]
-                    bboxsSequence   = bboxs  [batchId * NUM_TRUNCATE : (batchId + 1) * NUM_TRUNCATE + 1]
+                # Print information
+                print ('Load metadata of batch of objectIds in folder %s ...................' % (folderName))
+                for objectId in batchObjectIds:
+                    print ('    ObjectId = %d' % (objectId))
 
-                    # Extract feature and prepare bounding box before training....
-                    featureSequence = FeatureFactory.ExtractFeature(imsPath = imsPathSequence)   # Extract sequence features
-                    heatmapSequence = CreateHeatmapSequence(DefaultBboxs, bboxsSequence)
+                # Get information of batch of object ids
+                imsPathBatch = []
+                bboxsBatch   = []
+                maxSequence  = 0
+                for objectId in batchObjectIds:
+                    Dataset.DataOpts['data_object_id'] = objectId
+                    imsPath, bboxs = Dataset.GetSequenceBy(occluderThres=0.5)
+                    imsPathBatch.append(imsPath)
+                    bboxsBatch.append(bboxs)
+                    maxSequence = max(maxSequence, imsPath.__len__())
+                for (imsPath, bboxs) in zip(imsPathBatch, bboxsBatch):
+                    if imsPath.__len__() < maxSequence:
+                        for id in range(maxSequence - imsPath.__len__()):
+                            imsPath.append('')
+                            bboxs.append([0, 0, 0, 0])
 
-                    featureSequence  = featureSequence[0: NUM_TRUNCATE]
-                    heatmapXSequence = heatmapSequence[0: NUM_TRUNCATE]
-                    heatmapYSequence = heatmapSequence[1: NUM_TRUNCATE + 1]
-                    print ('\r Load mini sequence ! Done !')
+                # Reset status at the beginning of each sequence
+                S = startStateS; C = startStateC;
 
-                    print ('\r Train mini sequence !.....................')
+                # Iterate sequence of batch
+                numSequence  = (maxSequence - 1) // NUM_TRUNCATE
+                for sequenceIdx in range(numSequence):
+                    # Extract features of batch
+                    featureBatch  = numpy.zeros((BATCH_SIZE, NUM_TRUNCATE, 5461, 256), dtype='float32')
+                    heatmapXBatch = numpy.zeros((BATCH_SIZE, NUM_TRUNCATE, 5461, 1), dtype='float32')
+                    heatmapYBatch = numpy.zeros((BATCH_SIZE, NUM_TRUNCATE, 5461, 1), dtype='float32')
+
+                    for objectIdx in range(BATCH_SIZE):
+                        # Get sequence
+                        imsPathSequence = imsPathBatch[objectIdx][sequenceIdx * NUM_TRUNCATE: (sequenceIdx + 1) * NUM_TRUNCATE + 1]
+                        bboxsSequence   = bboxsBatch  [objectIdx][sequenceIdx * NUM_TRUNCATE: (sequenceIdx + 1) * NUM_TRUNCATE + 1]
+
+                        # Extract feature and prepare bounding box before training....
+                        featureSequence = FeatureFactory.ExtractFeature(imsPath = imsPathSequence, batchSize = NUM_TRUNCATE + 1)  # Extract sequence features
+                        heatmapSequence = CreateHeatmapSequence(DefaultBboxs, bboxsSequence)
+
+                        # for heatmap, bbox in zip(heatmapSequence, bboxsSequence):
+                        #     print ('%f %f %f %f %f' % (numpy.sum(heatmap), bbox[0], bbox[1], bbox[2], bbox[3]))
+
+                        featureSequence  = featureSequence[0: NUM_TRUNCATE]
+                        heatmapXSequence = heatmapSequence[0: NUM_TRUNCATE]
+                        heatmapYSequence = heatmapSequence[1: NUM_TRUNCATE + 1]
+
+                        featureBatch[objectIdx, :, :, :]  = featureSequence
+                        heatmapXBatch[objectIdx, :, :, :] = heatmapXSequence
+                        heatmapYBatch[objectIdx, :, :, :] = heatmapYSequence
+                    print ('    Load batch. Done !')
+
                     iter += 1
-                    cost, newS, newC = LSTMModel.TrainFunc(featureSequence,
-                                                           heatmapXSequence,
-                                                           heatmapYSequence,
-                                                           S, C)
-                    print ('\r Train mini sequence ! Done !')
-
-
+                    result = LSTMModel.TrainFunc(featureBatch,
+                                                 heatmapXBatch,
+                                                 heatmapYBatch,
+                                                 S, C)
+                    cost = result[0]
+                    newS = result[1 : 1 + BATCH_SIZE]
+                    newC = result[1 + BATCH_SIZE : 1 + 2 * BATCH_SIZE]
                     costs.append(cost)
+                    print ('    Train mini sequence in a batch ! Done !')
 
                     if iter % DISPLAY_FREQUENCY == 0:
                         # Print information of current training in progress
-                        print (INFO_DISPLAY % (epoch, iter, numpy.mean(costs), folderName, objectId))
+                        print (INFO_DISPLAY % (epoch, iter, numpy.mean(costs)))
 
                         # Plot result in progress
                         iterVisualize.append(iter)
@@ -284,45 +324,49 @@ def TrainModel():
                         file.close()
                         print ('Save model !')
 
-                    S = newS;       C = newC
+                    S = numpy.asarray(newS, dtype = 'float32');       C = numpy.asarray(newC, dtype = 'float32')
 
 def DrawHeatmap(imsPath, heatmaps):
-    fig, ax = plt.subplots(1)
+    fig, ax = plt.subplots(2, 4)
     ab = None
 
-    for imPath, heatmaps in zip(imsPath, heatmaps):
-        # rawIm = cv2.imread(imPath)
-
-        a = heatmaps[64 * 64 + 32 * 32:64 * 64 + 32 * 32 + 16 * 16]
-        a = numpy.asarray(a.reshape((16, 16)) * 255, dtype='uint8')
+    for heatmap in heatmaps:
+        idx = numpy.asarray([0, 64 * 64, 32 * 32, 16 * 16, 8 * 8, 4 * 4, 2 * 2, 1 * 1], dtype='int32')
+        idx = numpy.cumsum(idx)
+        win64 = heatmap[idx[0]: idx[1]];
+        win64 = win64.reshape((64, 64))
+        win32 = heatmap[idx[1]: idx[2]];
+        win32 = win32.reshape((32, 32))
+        win16 = heatmap[idx[2]: idx[3]];
+        win16 = win16.reshape((16, 16))
+        win8 = heatmap[idx[3]: idx[4]];
+        win8 = win8.reshape((8, 8))
+        win4 = heatmap[idx[4]: idx[5]];
+        win4 = win4.reshape((4, 4))
+        win2 = heatmap[idx[5]: idx[6]];
+        win2 = win2.reshape((2, 2))
+        win1 = heatmap[idx[6]: idx[7]];
+        win1 = win1.reshape((1, 1))
 
         if ab == None:
-            ab = ax.imshow(a)
+            ab2 = ax[0, 1].imshow(win64)
+            ab3 = ax[0, 2].imshow(win32)
+            ab4 = ax[0, 3].imshow(win16)
+            ab5 = ax[1, 0].imshow(win8)
+            ab6 = ax[1, 1].imshow(win4)
+            ab7 = ax[1, 2].imshow(win2)
+            ab8 = ax[1, 3].imshow(win1)
         else:
-            ab.set_data(a)
-
-
-        # img = Image.fromarray(a, 'GRAY')
-
-
-        # cx = bbox[0]
-        # cy = bbox[1]
-        # w = bbox[2]
-        # h = bbox[3]
-        #
-        # box = [cx - w / 2, cy - h / 2, cx + w / 2, cy + h / 2]
-        #
-        # h, w, _ = rawIm.shape
-        # rect = patches.Rectangle((box[0] * w, box[1] * h), (box[2] - box[0]) * w,
-        #                          (box[3] - box[1]) * h, linewidth=1, edgecolor='r', facecolor='none')
-        # # Add the patch to the Axes
-        # ax.add_patch(rect)
-
+            ab2.set_data(win64)
+            ab3.set_data(win32)
+            ab4.set_data(win16)
+            ab5.set_data(win8)
+            ab6.set_data(win4)
+            ab7.set_data(win2)
+            ab8.set_data(win1)
         plt.show()
         plt.axis('off')
-
-        plt.pause(0.05)
-
+        plt.pause(0.00001)
         # rect.remove()
 
 def Draw(imsPath, bboxs):
