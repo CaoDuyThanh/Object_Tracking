b66f2308bf549c47bc3ae8f277ac97a13aa9d0e8
caoduythanh <caoduythanhcantho@gmail.com>
2017-06-09 13:56:28 +0700

    Modified: - Add create ortho parameters - Add bound as parameter when random weight

--- Layers/UtilLayer.py
+++ Layers/UtilLayer.py
@@ -3,12 +3,14 @@ import theano
 
 def CreateSharedParameter(rng    = None,
                           shape  = None,
+                          wBound = None,
                           factor = 1,
                           nameVar = ''):
     if rng is None:
         rng = numpy.random.RandomState(1993)
 
-    wBound = numpy.sqrt(6.0 / numpy.sum(shape))
+    if wBound is None:
+        wBound = numpy.sqrt(6.0 / numpy.sum(shape))
     initValue = numpy.asarray(rng.uniform(
                                         low   = -wBound,
                                         high  =  wBound,
@@ -16,5 +18,17 @@ def CreateSharedParameter(rng    = None,
                                     ),
                                     dtype = theano.config.floatX
                             )
+    sharedVar = theano.shared(initValue * factor, borrow = True, name = nameVar)
+    return sharedVar
+
+def CreateOrthoParameter(rng    = None,
+                         shape  = None,
+                         factor = 1,
+                         nameVar = ''):
+    if rng is None:
+        rng = numpy.random.RandomState(1993)
+
+    initValue = numpy.asarray(rng.normal(size = shape),
+                              dtype = theano.config.floatX)
     sharedVar = theano.shared(initValue * factor, borrow = True, name = nameVar)
     return sharedVar
