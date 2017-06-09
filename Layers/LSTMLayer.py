debf2885fd3746523b779687681168f3964c4445
caoduythanh <caoduythanhcantho@gmail.com>
2017-06-09 13:55:11 +0700

    Modified: - Fix name of variable

--- Layers/LSTMLayer.py
+++ Layers/LSTMLayer.py
@@ -9,8 +9,9 @@ class LSTMLayer(BasicLayer):
                  net,
                  inputs,
                  ckm1,
-                 skm1):
+                 hkm1):
         # Save all information to its layer
+        self.NetName     = net.NetName
         self.NumHidden   = net.LayerOpts['lstm_num_hidden']
         self.InputsSize  = net.LayerOpts['lstm_inputs_size']
         self.OutputsSize = net.LayerOpts['lstm_outputs_size']
@@ -18,96 +19,82 @@ class LSTMLayer(BasicLayer):
 
         if self.Params is None:
             # Parameters for list of input
-            Uis = []; Ufs = []; Ucs = []; Uos = []            # Input Weight
+            Wis = []; Wfs = []; Wcs = []; Wos = []            # Input Weight
             for idx, inputSize in enumerate(self.InputsSize):
-                Ui = CreateSharedParameter(net.NetOpts['rng'], (inputSize, self.NumHidden), 1, 'lstm_Ui_%d' % (idx))
-                Uf = CreateSharedParameter(net.NetOpts['rng'], (inputSize, self.NumHidden), 1, 'lstm_Uf_%d' % (idx))
-                Uc = CreateSharedParameter(net.NetOpts['rng'], (inputSize, self.NumHidden), 1, 'lstm_Uc_%d' % (idx))
-                Uo = CreateSharedParameter(net.NetOpts['rng'], (inputSize, self.NumHidden), 1, 'lstm_Uo_%d' % (idx))
+                Wi = CreateSharedParameter(net.NetOpts['rng'], (inputSize, self.NumHidden), 0.08, 1, '%s_Wi_%d' % (self.NetName, idx))
+                Wf = CreateSharedParameter(net.NetOpts['rng'], (inputSize, self.NumHidden), 0.08, 1, '%s_Wf_%d' % (self.NetName, idx))
+                Wc = CreateSharedParameter(net.NetOpts['rng'], (inputSize, self.NumHidden), 0.08, 1, '%s_Wc_%d' % (self.NetName, idx))
+                Wo = CreateSharedParameter(net.NetOpts['rng'], (inputSize, self.NumHidden), 0.08, 1, '%s_Wo_%d' % (self.NetName, idx))
 
-                Uis.append(Ui)
-                Ufs.append(Uf)
-                Ucs.append(Uc)
-                Uos.append(Uo)
+                Wis.append(Wi)
+                Wfs.append(Wf)
+                Wcs.append(Wc)
+                Wos.append(Wo)
 
-            # Init Wi | Ui | bi
-            Wi = CreateSharedParameter(net.NetOpts['rng'], (self.NumHidden, self.NumHidden), 1, 'lstm_Wi')
-            bi = CreateSharedParameter(net.NetOpts['rng'], (self.NumHidden, )              , 0, 'lstm_bi')
+            # Init Ui | bi
+            Ui = CreateSharedParameter(net.NetOpts['rng'], (self.NumHidden, self.NumHidden), 0.08, 1, '%s_Ui' % (self.NetName))
+            bi = CreateSharedParameter(net.NetOpts['rng'], (self.NumHidden, )              , 0.08, 0, '%s_bi' % (self.NetName))
 
-            # Init Wf | bf
-            Wf = CreateSharedParameter(net.NetOpts['rng'], (self.NumHidden, self.NumHidden), 1, 'lstm_Wf')
-            bf = CreateSharedParameter(net.NetOpts['rng'], (self.NumHidden, )              , 0, 'lstm_bf')
+            # Init Uf | bf
+            Uf = CreateSharedParameter(net.NetOpts['rng'], (self.NumHidden, self.NumHidden), 0.08, 1, '%s_Uf' % (self.NetName))
+            bf = CreateSharedParameter(net.NetOpts['rng'], (self.NumHidden, )              , 0.08, 0, '%s_bf' % (self.NetName))
 
-            # Init Wc | bc
-            Wc = CreateSharedParameter(net.NetOpts['rng'], (self.NumHidden, self.NumHidden), 1, 'lstm_Wc')
-            bc = CreateSharedParameter(net.NetOpts['rng'], (self.NumHidden, )              , 0, 'lstm_bc')
+            # Init Uc | bc
+            Uc = CreateSharedParameter(net.NetOpts['rng'], (self.NumHidden, self.NumHidden), 0.08, 1, '%s_Uc' % (self.NetName))
+            bc = CreateSharedParameter(net.NetOpts['rng'], (self.NumHidden, )              , 0.08, 0, '%s_bc' % (self.NetName))
 
-            # Init Wo | bo
-            Wo = CreateSharedParameter(net.NetOpts['rng'], (self.NumHidden, self.NumHidden), 1, 'lstm_Wo')
-            bo = CreateSharedParameter(net.NetOpts['rng'], (self.NumHidden, )              , 0, 'lstm_bo')
+            # Init Uo | bo
+            Uo = CreateSharedParameter(net.NetOpts['rng'], (self.NumHidden, self.NumHidden), 0.08, 1, '%s_Uo' % (self.NetName))
+            bo = CreateSharedParameter(net.NetOpts['rng'], (self.NumHidden, )              , 0.08, 0, '%s_bo' % (self.NetName))
 
             # Parameters for list of output
             Wys = [];  bys = [];   # Output Weight | Bias
             for idx, outputSize in enumerate(self.OutputsSize):
-                Wy = CreateSharedParameter(net.NetOpts['rng'], (self.NumHidden, outputSize), 1, 'lstm_Wy_%d' % (idx))
-                by = CreateSharedParameter(net.NetOpts['rng'], (outputSize,), 0, 'lstm_by_%d' % (idx))
+                Wy = CreateSharedParameter(net.NetOpts['rng'], (self.NumHidden, outputSize), 0.08, 1, '%s_Wy_%d' % (self.NetName, idx))
+                by = CreateSharedParameter(net.NetOpts['rng'], (outputSize,), 0.08, 0, '%s_by_%d' % (self.NetName, idx))
 
                 Wys.append(Wy)
                 bys.append(by)
 
-            self.Params = Uis + Ufs + Ucs + Uos + \
+            self.Params = Wis + Wfs + Wcs + Wos + \
                           Wys + bys + \
-                          [Wi,  Wf,   Wc,   Wo] + \
+                          [Ui,  Uf,   Uc,   Uo] + \
                           [bi,  bf,   bc,   bo]
 
         # Get all weight from param
         numInputs  = self.InputsSize.__len__()
         numOutputs = self.OutputsSize.__len__()
-        Uis = self.Params[0 : numInputs]
-        Ufs = self.Params[numInputs * 1 : numInputs * 2]
-        Ucs = self.Params[numInputs * 2 : numInputs * 3]
-        Uos = self.Params[numInputs * 3 : numInputs * 4]
+        Wis = self.Params[0 : numInputs]
+        Wfs = self.Params[numInputs * 1 : numInputs * 2]
+        Wcs = self.Params[numInputs * 2 : numInputs * 3]
+        Wos = self.Params[numInputs * 3 : numInputs * 4]
         Wys = self.Params[numInputs * 4                  : numInputs * 4 + numOutputs * 1]
         bys = self.Params[numInputs * 4 + numOutputs * 1 : numInputs * 4 + numOutputs * 2]
-        Wi  = self.Params[numInputs * 4 + numOutputs * 2]
-        Wf  = self.Params[numInputs * 4 + numOutputs * 2 + 1]
-        Wc  = self.Params[numInputs * 4 + numOutputs * 2 + 2]
-        Wo  = self.Params[numInputs * 4 + numOutputs * 2 + 3]
+        Ui  = self.Params[numInputs * 4 + numOutputs * 2]
+        Uf  = self.Params[numInputs * 4 + numOutputs * 2 + 1]
+        Uc  = self.Params[numInputs * 4 + numOutputs * 2 + 2]
+        Uo  = self.Params[numInputs * 4 + numOutputs * 2 + 3]
         bi  = self.Params[numInputs * 4 + numOutputs * 2 + 4]
         bf  = self.Params[numInputs * 4 + numOutputs * 2 + 5]
         bc  = self.Params[numInputs * 4 + numOutputs * 2 + 6]
         bo  = self.Params[numInputs * 4 + numOutputs * 2 + 7]
 
-        # Slice inputs to smaller input
-        # slicedInput = []; startId = 0
-        # for inputSize in self.InputsSize:
-        #     slicedInput.append(inputs[startId : startId + inputSize])
-        #     startId += inputSize
-
-        # Calculate input for each i, f, o
-        # inputI = 0; inputF = 0; inputO = 0; inputG = 0
-        # for idx, inputSize in enumerate(self.InputsSize):
-        #     inputI += T.dot(slicedInput[idx], Uis[idx])
-        #     inputF += T.dot(slicedInput[idx], Ufs[idx])
-        #     inputO += T.dot(slicedInput[idx], Uos[idx])
-        #     inputG += T.dot(slicedInput[idx], Ucs[idx])
-
-        inputI = T.dot(inputs, Uis[0])
-        inputF = T.dot(inputs, Ufs[0])
-        inputO = T.dot(inputs, Uos[0])
-        inputG = T.dot(inputs, Ucs[0])
+        inputI = T.dot(inputs, Wis[0])
+        inputF = T.dot(inputs, Wfs[0])
+        inputO = T.dot(inputs, Wos[0])
+        inputG = T.dot(inputs, Wcs[0])
 
         # Calculate to next layer
-        i = T.nnet.sigmoid(inputI + T.dot(skm1, Wi) + bi)
-        f = T.nnet.sigmoid(inputF + T.dot(skm1, Wf) + bf)
-        o = T.nnet.sigmoid(inputO + T.dot(skm1, Wo) + bo)
-        g = T.tanh(inputG + T.dot(skm1, Wc) + bc)
+        i = T.nnet.sigmoid(inputI + T.dot(hkm1, Ui) + bi)
+        f = T.nnet.sigmoid(inputF + T.dot(hkm1, Uf) + bf)
+        o = T.nnet.sigmoid(inputO + T.dot(hkm1, Uo) + bo)
+        g = T.tanh(inputG + T.dot(hkm1, Uc) + bc)
 
         self.C = ckm1 * f + g * i
-        self.S = T.tanh(self.C) * o
+        self.H = T.tanh(self.C) * o
 
         # Calculate output
         output = []
         for Wy, by in zip(Wys, bys):
-            output.append(T.dot(self.S, Wy) + by)
+            output.append(T.dot(self.H, Wy) + by)
         self.Output = output
